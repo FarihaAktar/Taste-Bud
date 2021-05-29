d9547873ab7b1590b0564992356e5ecc0938e900
Farihaakter <yellowfariha@gmail.com>
2021-05-30 02:06:58 +0600

    added detail page and update redux

--- src/components/reactComponents/HomePage/MainSection/Lunch/Lunch.js
+++ src/components/reactComponents/HomePage/MainSection/Lunch/Lunch.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Lunch = () => {
     const lunch = useSelector((state)=> state.meals.lunchMenu)
     // console.log(breakfast)
     const renderMenu = lunch.map((meal)=>{
-        const {mealImageURL, mealName, price} = meal
+        const {mealImageURL, mealName, price, id} = meal
         return(
             <div className='breakfast-meal'>
+                <Link to={`/meal/${id}`}>
                 <img className='breakfast-image' src={mealImageURL} alt="" />
                 <h4 className='meal-name'>{mealName}</h4>
                 <p className='price'>${price}</p>
+                </Link>
             </div>
         )
     })
