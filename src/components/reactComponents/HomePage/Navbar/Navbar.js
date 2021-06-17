0becbba1f50d7f5bd2bd9a914b1ffbe5e0b41961
Farihaakter <yellowfariha@gmail.com>
2021-06-17 21:23:57 +0600

    change logo name

--- src/components/reactComponents/HomePage/Navbar/Navbar.js
+++ src/components/reactComponents/HomePage/Navbar/Navbar.js
@@ -4,12 +4,12 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1 className='logo'>Teste<span className='bud'>BUD</span></h1>
+        <nav className='nav'>
+            <h1 className='logo'>Taste<span className='bud'>BUD</span></h1>
             <ul className='nav-link'>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
                 <li><Link to="/admin">Admin</Link></li>
             </ul>
         </nav>
